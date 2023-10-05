d69757ac620264cddc19cf97ea8424b93d01b375
Emma Hopson <emmaehopson@icloud.com>
2023-10-04 18:37:57 -0600

    Initial Commit

---
+++ Assignment 6/Assignment_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_6App.swift
+//  Assignment 6
+//
+//  Created by Emma  Hopson on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
