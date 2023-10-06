fe477ecd9009538cf10bcf4d218e91ec65a5e949
Emma Hopson <emmaehopson@icloud.com>
2023-10-05 23:43:14 -0600

    Assignment 6

--- Assignment 6/Views/TimeDetailView.swift
+++ Assignment 6/Views/TimeDetailView.swift
@@ -48,7 +48,7 @@ struct TimeDetailView: View {
                         VStack{
                             Text("ERROR: COULD NOT LOAD TIME DETAILS")
                                 .foregroundColor(.red)
-                            Text("Instead, please enjoy this meme that Brodie made of Emma in January of 2023")
+                            Text("Instead, please enjoy this meme that Brodie made of Emma back in January")
                                 .frame(width: 300)
                             Image("ErrorMeme")
                             Text("This is how Emma feels after breaking the entire API server :)")
